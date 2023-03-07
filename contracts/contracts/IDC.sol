d8eeb311dbcbd02e860a8bbb150bb96b6c8158d1
polymorpher <aaron@qokka.ai>
2023-03-07 02:07:28 -0800

    fix tweet sol

--- contracts/contracts/IDC.sol
+++ contracts/contracts/IDC.sol
@@ -4,12 +4,12 @@ pragma solidity ~0.8.17;
 
 interface IDC {
     function available(string memory) external view returns (bool);
-    function makeCommitment(string memory, address, uint256, bytes32) external view returns (bytes32);
+    function duration() external view returns (uint256);
+    function makeCommitment(string memory, address, bytes32) external view returns (bytes32);
     function commit(bytes32 commitment) external; 
-    function getENSPrice(string memory, uint256) external view returns (uint256);
-    function register(string calldata, address, uint256, bytes32) external payable;
-    function renew(string calldata, uint256) external payable;
-    function verifyOnwer(string calldata, address) external view returns(bool);
+    function getPrice(string memory) external view returns (uint256);
+    function register(string calldata, address, bytes32) external payable;
+    function renew(string calldata) external payable;
     function nameExpires(string calldata) external view returns(uint256);
     function ownerOf(string calldata name) external view returns(address);
 }
