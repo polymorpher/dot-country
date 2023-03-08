6d193db2fc969eef8bb97bde9dfc57f729f8bb90
polymorpher <aaron@qokka.ai>
2023-03-08 00:45:04 -0800

    fix env and config

--- contracts/config.ts
+++ contracts/config.ts
@@ -4,11 +4,14 @@ dotenv.config()
 
 export default {
   debug,
+  // Tweet only
   baseRentalPrice: process.env.BASE_RENTAL_PRICE_ETH || '10',
   revenueAccount: process.env.REVENUE_ACCOUNT,
+  dc: process.env.DC_CONTRACT,
+
+  // DC
   registrarController: process.env.REGISTRAR_CONTROLLER,
   nameWrapper: process.env.NAME_WRAPPER,
-  dc: process.env.DC_CONTRACT,
   registrar: process.env.REGISTRAR,
   duration: parseFloat(process.env.DURATION_DAYS || '90'),
   resolver: process.env.RESOLVER,
