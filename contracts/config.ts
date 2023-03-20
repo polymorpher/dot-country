6023a568ac4f2e576f3bb3bbe71c75d89505fb88
polymorpher <aaron@qokka.ai>
2023-03-20 01:25:10 -0700

    DCDeployer contract and script

--- contracts/config.ts
+++ contracts/config.ts
@@ -4,18 +4,21 @@ dotenv.config()
 
 export default {
   debug,
+  // Deployer
+  multisig: process.env.MULTISIG_ACCOUNT as string,
+
   // Tweet only
   baseRentalPrice: process.env.BASE_RENTAL_PRICE_ETH || '10',
-  revenueAccount: process.env.REVENUE_ACCOUNT,
+  revenueAccount: process.env.REVENUE_ACCOUNT as string,
   dc: process.env.DC_CONTRACT,
 
   // DC
-  registrarController: process.env.REGISTRAR_CONTROLLER,
-  nameWrapper: process.env.NAME_WRAPPER,
-  registrar: process.env.REGISTRAR,
+  registrarController: process.env.REGISTRAR_CONTROLLER as string,
+  nameWrapper: process.env.NAME_WRAPPER as string,
+  registrar: process.env.REGISTRAR as string,
   duration: parseFloat(process.env.DURATION_DAYS || '90'),
-  resolver: process.env.RESOLVER,
-  reverseRecord: process.env.REVERSE_RECORD || true,
+  resolver: process.env.RESOLVER as string,
+  reverseRecord: process.env.REVERSE_RECORD === 'true',
   fuses: parseInt(process.env.FUSES || '0'),
   initialRecordFile: process.env.INITIAL_RECORD_FILE
 }
